befed6505b753e1dde5c8bf98db91fc372238a4b
Karup <kperca802@gmail.com>
2025-08-07 18:40:52 -0600

    Clase Libros

--- Actividad 16.py
+++ Actividad 16.py
@@ -1,1 +1,9 @@
-print("---BIBLIOTECA---\n")
+class Libros:
+    def __init__(self, titulo, autor, anio):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio = anio
+    def mostrar_libros(self):
+        print(f"Título: {self.titulo}\n"
+              f"Autor: {self.autor}\n"
+              f"Año de publicación: {self.anio}")
