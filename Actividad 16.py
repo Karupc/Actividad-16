249aed6688134c6a2b095bfdfecd668e107ca733
Karup <kperca802@gmail.com>
2025-08-07 19:30:44 -0600

    Función mostrar libros

--- Actividad 16.py
+++ Actividad 16.py
@@ -3,7 +3,7 @@ class Libros:
         self.titulo = titulo
         self.autor = autor
         self.anio = anio
-    def mostrar_libros(self):
+    def mostrar_lib(self):
         print(f"Título: {self.titulo}\n"
               f"Autor: {self.autor}\n"
               f"Año de publicación: {self.anio}")
@@ -14,12 +14,20 @@ def agregar_libros():
         titulo = input("Ingresar el título del libro: ")
         autor = input("Ingresar el autor del libro: ")
         anio = int(input("Ingresar el año de publicación: "))
-        libro.append(titulo, autor, anio)
+        libro = Libros(titulo, autor, anio)
         lista_libros.append(libro)
+        print("")
     except ValueError:
         print("Error: El año debe ser un número entero.\n")
 def mostrar_libros():
-
+    if not lista_libros:
+        print("No hay libros registrados.\n")
+    else:
+        print("--- Lista de libros ---")
+        for i, libro in enumerate(lista_libros, start=1):
+            print(f"{i}.- ", end="")
+            libro.mostrar_lib()
+print("\n")
 while True:
     print("---BIBLIOTECA---\n"
           "1.- Agregar libros\n"
@@ -34,3 +42,4 @@ while True:
             agregar_libros()
         case "2":
             print("---MOSTRAR LA LISTA DE LIBROS---")
+            mostrar_libros()
